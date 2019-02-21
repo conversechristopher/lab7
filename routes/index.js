f02ab99299ce2714f5f527d81a342a7df58521e6
Christopher Converse <cyconver@ucsd.edu>
2019-02-21 10:09:50 -0800

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects['viewAlt'] = false;
+	console.log("view");
   	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response) {
+	projects['viewAlt'] = true;
+	console.log("viewAlt");
+	response.render('index', projects);
+}
